2eaff0eadc64fb7da211c928d3af17bbf9265a3d
MatheusMagga <mathmagga@gmail.com>
2019-12-09 13:15:04 -0300

    Modificacao

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+
+export default function Header(){
+    return(
+        <View style={styles.container}
+            
+        />
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+      alignItems: 'center',
+      padding: 40
+    }
+  });
+
---
+++ src/components/Tabs/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+
+export default function Tabs(){
+    return(
+        <View style={styles.container}
+            
+        />
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#8B10AE'
+    }
+  });
+
